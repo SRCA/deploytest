5330780366c60d060b4f9cba262636c825942d54
David Jacobo <djacobos@yahoo.com>
2020-04-29 12:01:04 -0500

    First deploy

---
+++ deploytest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(deploytest.Startup))]
+namespace deploytest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
